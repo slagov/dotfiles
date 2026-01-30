47cfcb16813cff40d2b54e9e86294a9d69903a1a
Sergey Lagov <lagovsp@gmail.com>
2026-01-30 06:29:55 +0300

    updates config files

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,7 +5,11 @@ config.font_size = 15
 config.line_height = 0.8
 
 config.hide_tab_bar_if_only_one_tab = true
-config.color_scheme = 'Tokyo Night'
+-- config.color_scheme = 'Tokyo Night'
+config.color_scheme = 'Red Scheme'
+
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
 
 -- config.term = 'wezterm' -- или 'xterm-256color' для максимальной совместимости
 
