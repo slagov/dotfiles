1878226d299504428feccb52245c00b53dded39c
Sergey Lagov <lagovsp@gmail.com>
2026-02-01 01:02:34 +0300

    updates config files

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,25 +5,37 @@ config.font_size = 15
 config.line_height = 0.8
 
 config.hide_tab_bar_if_only_one_tab = true
--- config.color_scheme = 'Tokyo Night'
-config.color_scheme = 'Red Scheme'
+config.color_scheme = 'Adventure Time (Gogh)'
+-- config.color_scheme = 'Cyberdyne'
+-- config.color_scheme = 'Apple System Colors'
 
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
 
--- config.term = 'wezterm' -- или 'xterm-256color' для максимальной совместимости
+config.initial_cols = 100
+config.initial_rows = 30
 
 config.keys = {
     {
-      key = 't',
-      mods = 'CTRL',
-      action = wezterm.action.SpawnTab 'CurrentPaneDomain',
+        key = 't',
+        mods = 'CTRL',
+        action = wezterm.action.SpawnTab 'CurrentPaneDomain',
     },
     {
-    key = 'x',
-    mods = 'CTRL',
-    action = wezterm.action.CloseCurrentTab { confirm = false },
-  }
+        key = 'x',
+        mods = 'CTRL',
+        action = wezterm.action.CloseCurrentTab { confirm = false },
+    },
+    {
+        key = 'Tab',
+        mods = 'CTRL',
+        action = wezterm.action.ActivateTabRelative(1),
+    },
+    {
+        key = 'Tab',
+        mods = 'CTRL|SHIFT',
+        action = wezterm.action.ActivateTabRelative(-1),
+    },
 }
 
 return config
--- .zshrc
+++ .zshrc
@@ -3,7 +3,10 @@
 
 export ZSH="$HOME/.oh-my-zsh"
 
-ZSH_THEME="agnosterzak"
+# ZSH_THEME="duellj"
+ZSH_THEME="jonathan"
+# ZSH_THEME="rkj-repos"
+# ZSH_THEME="xiong-chiamiov-plus"
 
 plugins=( 
     git
