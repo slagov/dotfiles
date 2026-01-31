ad1db13272f614dbe66944e05d257fe4e7632efc
Sergey Lagov <lagovsp@gmail.com>
2026-02-01 01:30:45 +0300

    updates config files

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# add repositories
+curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
+echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
+sudo chmod 644 /usr/share/keyrings/wezterm-fury.gpg
+
+sudo apt update
+
+# install zsh
+sudo apt install -y zsh
+
+# install ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install wezterm-nightly
+sudo apt install wezterm-nightly
