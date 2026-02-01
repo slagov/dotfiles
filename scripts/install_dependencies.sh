c755c46f68f52449a0be63db14e9194431e03908
Sergey Lagov <lagovsp@gmail.com>
2026-02-01 04:03:36 +0300

    updates config files

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
+sudo apt update
+sudo apt install -y curl stow
+
 # add repositories
 curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
 echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
 sudo chmod 644 /usr/share/keyrings/wezterm-fury.gpg
 
 sudo apt update
-
-sudo apt install -y curl stow
 
 # install zsh
 sudo apt install -y zsh
