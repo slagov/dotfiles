6069f622dd2e1df67bf1dd8b901d3c9295f9a227
Sergey Lagov <lagovsp@gmail.com>
2026-02-01 04:58:33 +0300

    updates config files

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -14,7 +14,7 @@ sudo apt update
 sudo apt install -y zsh
 
 # install ohmyzsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended # CHSH=no
 
 # install wezterm-nightly
 sudo apt install -y wezterm-nightly
